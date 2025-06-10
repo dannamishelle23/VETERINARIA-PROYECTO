3b46559f3fec0948fbc9889757e1ef95dce70631
dannamishelle23 <dannamishelle.23@gmail.com>
2025-06-10 16:24:08 -0500

    Add files via upload

---
+++ frontend/src/hooks/useFetch.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+function useFetch() {
+    const fetchDataBackend = async (url, data = null, method = "GET",headers = {}) => {
+        const loadingToast = toast.loading("Cargando, espere...");
+        try {
+            const options = {
+            method,
+            url,
+            headers: {
+                "Content-Type": "application/json",
+                ...headers,
+            },
+            data,
+            }
+            const response = await axios(options)
+            toast.dismiss(loadingToast); 
+            toast.success(response?.data?.msg)
+            return response?.data
+        } catch (error) {
+            toast.dismiss(loadingToast); 
+            console.error(error)
+            toast.error(error.response?.data?.msg)
+        }
+    }
+
+    return { fetchDataBackend }
+}
+
+export default useFetch;
